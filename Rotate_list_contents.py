6b6ac764d80ea7613c2cf03041804405c15cdc7f
sraonekumar <msraonekumar@gmail.com>
2021-05-12 15:50:54 +0530

    RoatateListContents file added

---
+++ Rotate_list_contents.py
@@ -0,0 +1,25 @@
+def rotate(l,r,d = 0):
+    if(r > len(l)):
+        while(r > len(l)):
+            r = r% len(l)
+    if r ==0:
+        return l
+    else:
+        if(d == 0):
+            temp = list(l[r:])
+            temp.extend(l[:r])
+            return(temp)
+        else:
+            temp = list(l[:r])
+            temp.extend(l[r:])
+            return(temp)
+            
+        
+l = [1,2,3,4,5]
+print(l)
+print("left rotate:" ,rotate(l,10,0)) # d = -1 -- >right
+print("right rotate:",rotate(l,10,-1)) # d = 0 --> left
+
+
+    
+    
